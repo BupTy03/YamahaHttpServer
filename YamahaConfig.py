aedfa81e2ca705071d24c4eacc76c58fe88010c5
Anton <anton.lagosha.99@mail.ru>
2020-11-28 14:12:46 +0500

    + CD added + system/getFeatures added

--- YamahaConfig.py
+++ YamahaConfig.py
@@ -3,6 +3,10 @@ import time
 from pathlib import Path
 
 
+def to_boolean(str):
+    return str == "true"
+
+
 class YamahaConfig(object):
 
     def __init__(self, file_name):
@@ -28,6 +32,46 @@ class YamahaConfig(object):
 
             file.write(json.dumps(self.data, indent=4))
 
+    def _toggle_netusb_repeat(self):
+        next_state = {
+            "off": "one",
+            "one": "all",
+            "all": "off"
+        }
+
+        self.data["play_info"]["netusb"]["repeat"] = next_state[self.data["play_info"]["netusb"]["repeat"]]
+
+    def _toggle_cd_repeat(self):
+        next_state = {
+            "off": "one",
+            "one": "all",
+            "all": "folder",
+            "folder": "a-b",
+            "a-b": "off"
+        }
+
+        self.data["play_info"]["cd"]["repeat"] = next_state[self.data["play_info"]["cd"]["repeat"]]
+
+    def _toggle_netusb_shuffle(self):
+        next_state = {
+            "off": "on",
+            "on": "songs",
+            "songs": "albums",
+            "albums": "off"
+        }
+
+        self.data["play_info"]["netusb"]["shuffle"] = next_state[self.data["play_info"]["netusb"]["shuffle"]]
+
+    def _toggle_cd_shuffle(self):
+        next_state = {
+            "off": "on",
+            "on": "folder",
+            "folder": "program",
+            "program": "off"
+        }
+
+        self.data["play_info"]["cd"]["shuffle"] = next_state[self.data["play_info"]["cd"]["shuffle"]]
+
     def read_field(self, field):
         self._update_from_file()
         if not self.data:
@@ -46,33 +90,45 @@ class YamahaConfig(object):
         self.data["zones_info"][zone_name][param_name] = param_value
         self._store_to_file()
 
-    def toggle_repeat(self):
+    def read_play_info(self, play_info_type):
         self._update_from_file()
-        repeat = self.data["netusb_play_info"]["repeat"]
-        if repeat == "one":
-            self.data["netusb_play_info"]["repeat"] = "off"
-        elif repeat == "off":
-            self.data["netusb_play_info"]["repeat"] = "one"
+        return self.data["play_info"][play_info_type]
+
+    def toggle_repeat(self, play_info_type):
+        self._update_from_file()
+
+        if "netusb" == play_info_type:
+            self._toggle_netusb_repeat()
+        elif "cd" == play_info_type:
+            self._toggle_cd_repeat()
+        else:
+            assert False, "Wrong play_info_type {} for toggle_repeat".format(play_info_type)
 
         self._store_to_file()
 
-    def toggle_shuffle(self):
+    def toggle_shuffle(self, play_info_type):
         self._update_from_file()
-        shuffle = self.data["netusb_play_info"]["shuffle"]
-        if shuffle == "on":
-            self.data["netusb_play_info"]["shuffle"] = "off"
-        elif shuffle == "off":
-            self.data["netusb_play_info"]["shuffle"] = "on"
+
+        if "netusb" == play_info_type:
+            self._toggle_netusb_shuffle()
+        elif "cd" == play_info_type:
+            self._toggle_cd_shuffle()
+        else:
+            assert False, "Wrong play_info_type {} for toggle_shuffle".format(play_info_type)
 
         self._store_to_file()
 
-    def set_playback(self, playback):
+    def set_playback(self, play_info_type, playback):
         self._update_from_file()
-        self.data["netusb_play_info"]["playback"] = playback
+        self.data["play_info"][play_info_type]["playback"] = playback
         self._store_to_file()
 
     def set_switch_preset(self, switch_preset):
         self._update_from_file()
-        self.data["tuner_play_info"]["switch_preset"] = switch_preset
+        self.data["play_info"]["tuner"]["switch_preset"] = switch_preset
         self._store_to_file()
+
+    def get_system_features(self):
+        self._update_from_file()
+        return self.data["system"]
 
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from YamahaConfig import YamahaConfig
+from YamahaConfig import YamahaConfig, to_boolean
 
 import urllib
 import json
 import re
 
 
-def to_boolean(str):
-    return str == "true"
-
-
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def __init__(self, request, client_address, server):
         self.config = YamahaConfig("config.json")
-        self.zoneNameRegExp = re.compile("/([^/]+)/[^/]+$")
+        self.inputOrZoneNameRegExp = re.compile("/([^/]+)/[^/]+$")
         super().__init__(request, client_address, server)
 
-    def _get_zone_name(self, path, reg_exp):
-        groups = re.findall(reg_exp, path)
+    def _get_zone_name(self, path):
+        groups = re.findall(self.inputOrZoneNameRegExp, path)
         if groups and len(groups) > 0:
             return groups[0]
         return "main"
+
+    def _get_input_type(self, path):
+        groups = re.findall(self.inputOrZoneNameRegExp, path)
+        if groups and len(groups) > 0:
+            return groups[0]
+        return ""
 
     def _send_json(self, json_answer):
         self.send_response(200)
@@ -29,46 +31,52 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps(json_answer).encode('utf-8'))
 
-    def do_GET(self):
+    def _make_response(self):
         parsed_path = urllib.parse.urlparse(self.path)
         if parsed_path.path.endswith("getLocationInfo"):
             self._send_json(self.config.read_field("location_info"))
         elif parsed_path.path.endswith("getStatus"):
-            zone_name = self._get_zone_name(parsed_path.path, self.zoneNameRegExp)
+            zone_name = self._get_zone_name(parsed_path.path)
             self._send_json(self.config.zone_info(zone_name))
+
+        elif parsed_path.path.endswith("cd/getPlayInfo"):
+            self._send_json(self.config.read_play_info("cd"))
         elif parsed_path.path.endswith("tuner/getPlayInfo"):
-            self._send_json(self.config.read_field("tuner_play_info"))
+            self._send_json(self.config.read_play_info("tuner"))
         elif parsed_path.path.endswith("netusb/getPlayInfo"):
-            self._send_json(self.config.read_field("netusb_play_info"))
+            self._send_json(self.config.read_play_info("netusb"))
+        elif parsed_path.path.endswith("system/getFeatures"):
+            self._send_json(self.config.get_system_features())
+
         elif parsed_path.path.endswith("setInput"):
-            zone_name = self._get_zone_name(parsed_path.path, self.zoneNameRegExp)
+            zone_name = self._get_zone_name(parsed_path.path)
             input_name = parsed_path.query.split('=')[1]
             self.config.set_zone_param(zone_name, "input", input_name)
             self._send_json({"response_code": 0})
         elif parsed_path.path.endswith("setMute"):
-            zone_name = self._get_zone_name(parsed_path.path, self.zoneNameRegExp)
+            zone_name = self._get_zone_name(parsed_path.path)
             mute = to_boolean(parsed_path.query.split('=')[1])
             self.config.set_zone_param(zone_name, "mute", mute)
             self._send_json({"response_code": 0})
         elif parsed_path.path.endswith("setVolume"):
-            zone_name = self._get_zone_name(parsed_path.path, self.zoneNameRegExp)
+            zone_name = self._get_zone_name(parsed_path.path)
             volume = int(parsed_path.query.split('=')[1])
             self.config.set_zone_param(zone_name, "volume", volume)
             self._send_json({"response_code": 0})
         elif parsed_path.path.endswith("setPower"):
-            zone_name = self._get_zone_name(parsed_path.path, self.zoneNameRegExp)
+            zone_name = self._get_zone_name(parsed_path.path)
             power_mode = parsed_path.query.split('=')[1]
             self.config.set_zone_param(zone_name, "power", power_mode)
             self._send_json({"response_code": 0})
         elif parsed_path.path.endswith("toggleRepeat"):
-            self.config.toggle_repeat()
+            self.config.toggle_repeat(self._get_input_type(parsed_path.path))
             self._send_json({"response_code": 0})
         elif parsed_path.path.endswith("toggleShuffle"):
-            self.config.toggle_shuffle()
+            self.config.toggle_shuffle(self._get_input_type(parsed_path.path))
             self._send_json({"response_code": 0})
         elif parsed_path.path.endswith("setPlayback"):
             playback = parsed_path.query.split('=')[1]
-            self.config.set_playback(playback)
+            self.config.set_playback(self._get_input_type(parsed_path.path), playback)
             self._send_json({"response_code": 0})
         elif parsed_path.path.endswith("switchPreset"):
             switch_preset = parsed_path.query.split('=')[1]
@@ -78,6 +86,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             print("Unknown request: {}".format(parsed_path))
             self.send_response(404)
             self.end_headers()
+
+    def do_GET(self):
+        try:
+            self._make_response()
+        except Exception as e:
+            print("Exception: {}".format(e))
+            self.send_response(400)
+            self.end_headers()
+
 
 
 def main():
